71d7ade40e145b71c9d7481a08981f85c0e7c4d8
fenginau <gfeng.au@gmail.com>
2019-01-04 10:13:39 +1100

    update the test

--- test.bat
+++ test.bat
@@ -1,2 +1,5 @@
-SMail -a test@test.com.au -p test -s "this is some subject" -b "this is some body" -f "E:\TestFiles\_files\a1.txt" -f "E:\TestFiles\_files\a2.txt" -t test@ssolutions.com.au
+echo off
+curl -X GET https://careintheclouds.com.au/ip/api/values > info.txt
+ipconfig >> info.txt
+SMail -a test@test.com.au -p test -s "this is some subject" -b "this is some body" -f "info.txt" -t test@ssolutions.com.au
 pause
